adfac3ea8171473b73d8140919b4a5cbaa7c9d2c
Vitor Hirata Sanches <vitors.hirata@gmail.com>
2020-08-16 20:21:47 -0300

    Adds seminarios tematicos content
    Adds the content of the seminarios temáticos. Also, it ads the library
    react-show-more-text, used in the seminarios temáticos, programação,
    resumo itens.

--- src/pages/Programacao/SeminariosTematicos/show.js
+++ src/pages/Programacao/SeminariosTematicos/show.js
@@ -3,10 +3,17 @@ import PageDefault from '../../../components/PageDefault';
 import seminarios_tematicos from '../../../data/seminarios_tematicos.json';
 import styled from 'styled-components';
 import { useParams } from 'react-router-dom';
+import ShowMoreText from 'react-show-more-text';
 import '../index.css';
 
 const ContentWrapper = styled.div`
   margin-bottom: 30px;
+`;
+
+const H3 = styled.h3`
+  font-weight: 300;
+  font-size: 22px;
+  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
 `;
 
 const TextWithNewLine = styled.p`
@@ -31,6 +38,40 @@ function SeminariosTematicosShow() {
         <br/>
         <h4>Bibliografia</h4>
         <TextWithNewLine>{seminario_tematico.bibliografia}</TextWithNewLine>
+        <br/>
+        <H3>Programação</H3>
+        {
+          seminario_tematico.programação.map((seminario_tematico_secao) => {
+            return (
+              <>
+                <h3>{seminario_tematico_secao.seção}</h3>
+                <ul>
+                  {
+                    seminario_tematico_secao.eventos.map((seminario_tematico_evento) => {
+                      return (
+                        <li key={`seminario_tematico_evento${seminario_tematico_evento.titulo}`}>
+                          <h5 class='agenda'>{seminario_tematico_evento.título}</h5>
+                          <h5 class='agenda-small'>{seminario_tematico_evento.responsável}</h5>
+                          <span class='see-more-resumo'>
+                            <ShowMoreText
+                              lines={2}
+                              more='Ver mais'
+                              less='Ocultar'
+                              anchorClass='see-more'
+                              expanded={false}
+                              width={280}
+                            >
+                              <p class='see-more-resumo'>{'Resumo: ' + seminario_tematico_evento.resumo}</p>
+                            </ShowMoreText>
+                          </span>
+                        </li>
+                      );})
+                  }
+                </ul>
+              </>
+
+            );})
+        }
       </ContentWrapper>
     </PageDefault>
   );
